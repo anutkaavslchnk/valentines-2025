3bbd61e1a63c3de034f8dce8cdb15a5916a00439
Anna Vasylchenko <anutochkaaaaa14@gmail.com>
2025-01-27 22:38:00 +0100

    changes

--- src/app/components/valentine.tsx
+++ src/app/components/valentine.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaHeart } from 'react-icons/fa';
 export interface ValentineProps {
   children: React.ReactNode;
   action?: React.ReactNode;
@@ -6,7 +7,16 @@ export interface ValentineProps {
 export default function Valentine({}: ValentineProps) {
   return (
 <div className="flex items-center justify-center h-screen">
-  <div className="border border-black p-5 w-64 h-64"></div>
+  <div className="border border-transparent p-5 w-1/2 h-1/2 shadow-xl">
+  <div className='flex justify-center gap-2'>
+
+  <FaHeart className="" fill="red" size="30"/>
+  <FaHeart className="" fill="red" size="30"/>
+  <FaHeart className="" fill="red" size="30"/>
+  <FaHeart className="" fill="red" size="30"/>
+  </div>
+<h1>Will you be my valentine?</h1>
+  </div>
 </div>
 
   );
